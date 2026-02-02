51c36df20d75e7981179ddf470cbbf0514235e19
profniltonbarros <prof.niltonbarros@gmail.com>
2026-02-02 14:43:36 -0300

    Meu primeiro projeto Spring no GitHub

---
+++ src/main/java/com/ianes/ianes/IanesApplication.java
@@ -0,0 +1,13 @@
+package com.ianes.ianes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IanesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IanesApplication.class, args);
+	}
+
+}
